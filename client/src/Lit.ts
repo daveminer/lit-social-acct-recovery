b70d5f4f9a89d9c5e8df9bc0f95698809a0f25d3
daveminer <dave.miner@live.com>
2023-01-17 12:35:22 -0500

    add types to Lit.ts add prettier

--- client/src/Lit.ts
+++ client/src/Lit.ts
@@ -1,78 +1,110 @@
-import { accountRecovery } from './accessControl'
-import LitJsSdk from '@lit-protocol/sdk-browser'
+import { accountRecovery } from "./accessControl";
+import LitJsSdk, { LitNodeClient } from "@lit-protocol/sdk-browser";
+
+interface LitNodeKeys {
+  serverPubKey: string;
+  subnetPubKey: string;
+  networkPubKey: string;
+  networkPubKeySet: string[];
+}
+
+interface LitNodeClient {
+  config: object;
+  connect: Function;
+  connectedNodes: string[];
+  getEncryptionKey: Function;
+  ready: boolean;
+  networkPubKey: string;
+  networkPubKeySet: string[];
+  serverKeys: LitNodeKeys;
+  saveEncryptionKey: Function;
+  subnetPubKey: string;
+}
+
+interface EncryptionResult {
+  data: string;
+  symmetricKey: EncryptedSymmetricKey;
+}
+
+interface EncryptedSymmetricKey {
+  accessControlConditions: object[];
+  encryptedSymmetricKey: string;
+}
 
 class Lit {
-  authSig = undefined
-  chain = import.meta.env.VITE_CHAIN
-  litNodeClient = new LitJsSdk.LitNodeClient()
+  chain: string = import.meta.env.VITE_CHAIN;
+  litNodeClient: LitNodeClient = new LitJsSdk.LitNodeClient();
 
   async connect() {
-    console.log(this.chain, "CHAIN")
-    await this.litNodeClient?.connect()
+    await this.litNodeClient?.connect();
   }
 
-  // Prove user ownership of wallet to Lit nodes; required before using other SDK actions.
-  async getAuthSig() {
-    const authSig = await LitJsSdk.checkAndSignAuthMessage({
-      chain: this.chain,
-    })
-    this.authSig = authSig
-
-    return authSig
+  // Prove user ownership of wallet to Lit nodes.
+  // Required before using other SDK actions.
+  async getAuthSig(): Promise<string> {
+    return LitJsSdk.checkAndSignAuthMessage({ chain: this.chain });
   }
 
   // Encrypt arbitrary text and the key used to encrypt it. Returns encrypted
   // version of both.
-  async encrypt(text: string) {
-    const accessControlConditions = accountRecovery()
-    const { encryptedString, symmetricKey } = await LitJsSdk.encryptString(text)
+  async encryptSecret(text: string): Promise<EncryptionResult> {
+    const { encryptedString, symmetricKey } = await LitJsSdk.encryptString(
+      text
+    );
 
-    const authSig = await this.getAuthSig()
-
-    const symmKeyBytes = await this.litNodeClient.saveEncryptionKey({
-      accessControlConditions,
-      symmetricKey,
-      authSig,
-      chain: this.chain,
-    })
-
-    const encryptedSymmetricKey = LitJsSdk.uint8arrayToString(
-      symmKeyBytes,
-      'base16'
-    )
+    const { encryptedSymmetricKey, accessControlConditions } =
+      await this.encryptSymmetricKey(symmetricKey);
 
     return {
-      encryptedString,
-      encryptedSymmetricKey,
-      accessControlConditions
-    }
+      data: encryptedString,
+      symmetricKey: {
+        encryptedSymmetricKey,
+        accessControlConditions,
+      },
+    };
   }
 
   // Decrypt an encryptedString by providing the encryptedSymmetricKey and
   // passing the accessControlConditions.
-  async decrypt(encryptedString: string, encryptedSymmetricKey: string) {
+  async decryptSecret(encryptedString: string, encryptedSymmetricKey: string) {
     if (!this.litNodeClient) {
-      await this.connect()
+      await this.connect();
     }
 
     const authSig = await LitJsSdk.checkAndSignAuthMessage({
       chain: this.chain,
-    })
+    });
 
     const symmetricKey = await this.litNodeClient.getEncryptionKey({
       accessControlConditions: accountRecovery(),
       toDecrypt: encryptedSymmetricKey,
       chain: this.chain,
       authSig,
-    })
+    });
 
-    const decryptedString = await LitJsSdk.decryptString(
-      encryptedString,
-      symmetricKey
-    )
+    return await LitJsSdk.decryptString(encryptedString, symmetricKey);
+  }
 
-    return { decryptedString }
+  private async encryptSymmetricKey(
+    symmetricKey: string
+  ): Promise<EncryptedSymmetricKey> {
+    const authSig = await this.getAuthSig();
+    const accessControlConditions: object[] = accountRecovery();
+
+    const symmKeyBytes = await this.litNodeClient.saveEncryptionKey({
+      accessControlConditions,
+      symmetricKey,
+      authSig,
+      chain: this.chain,
+    });
+
+    const encryptedSymmetricKey = LitJsSdk.uint8arrayToString(
+      symmKeyBytes,
+      "base16"
+    );
+
+    return { encryptedSymmetricKey, accessControlConditions };
   }
 }
 
-export default new Lit()
+export default new Lit();
